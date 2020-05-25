ea8561027bcf18e5f27b9edc477149cf8c28056e
Geovanny <williamnavarrocanon@gmail.com>
2020-05-25 04:38:09 -0500

    add spinner and update files

--- js/main.js
+++ js/main.js
@@ -2,17 +2,51 @@ import dataApi from './api.js';
 
 const button = document.getElementById('action');
 const URL = 'https://rickandmortyapi.com/api/character';
-//characters less 19
-const id = 1;
-// pages less 29
-const pages = 2;
+const idRandom = () => {
+    return Math.floor(Math.random() * 20);
+}
+const pageRandom = () => {
+    return Math.floor(Math.random() * 30);
+}
+const showImg = false;
 
 function main() {
-    getData();
+    pressButton();
+    imgFirstLoadPage();
+
+
 }
 
-async function getData() {
+function imgFirstLoadPage() {
+    document.getElementById('loader').style.display = 'none';
+    // document.getElementById('cardInfo').style.display = 'none';
+
+    cardImg.innerHTML = `
+        <img alt="..." class="card-img-top" src=" ../assets/img/eyes.png">
+    `
+}
+
+function pressButton() {
+    button.onclick = () => {
+        document.getElementById('cardInfo').style.display = 'none';
+        document.getElementById('loader').style.display = 'inline';
+        document.getElementById('button').style.display = 'none';
+        setTimeout(function () {
+            showDataInView(URL, idRandom(), pageRandom());
+        }, 1000);
+    }
+}
+
+async function showDataInView(URL, id, pages) {
+
     const data = await dataApi(`${URL}/?page=${pages}`);
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('cardInfo').style.display = 'inline';
+    document.getElementById('button').style.display = 'inline';
+    conditionImageDeadOrAliveFilter(data, id);
+    cardImg.innerHTML = `
+        <img alt="..." class="card-img-top" src=" ${data.results[id].image}">
+    `
     title.innerHTML = `
         <h5 class="card-title ">${data.results[id].name}</h5>
     `
@@ -21,9 +55,15 @@ async function getData() {
         <li class="list-group-item" style="padding: 0.2rem ">Specie: ${data.results[id].species}</li>
         <li class="list-group-item" style="padding: 0.2rem ">Type: ${data.results[id].type}</li>
     `
-    cardImg.innerHTML = `
-        <img alt="..." class="card-img-top" src=" ${data.results[id].image}">
-    `
 }
+
+function conditionImageDeadOrAliveFilter(data, id) {
+    if (data.results[id].status == 'Dead') {
+        document.getElementById('cardImg').style.filter = 'grayscale(100%)';
+    } else {
+        document.getElementById('cardImg').style.filter = 'grayscale(0%)';
+    }
+}
+
 
 main();
